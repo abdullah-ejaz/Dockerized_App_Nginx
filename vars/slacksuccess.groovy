e77c9d7c00370b55630566ed2bad9803a19e38f7
Abdullah <abdullahejaz78601@gmail.com>
2021-07-08 03:56:10 +0500

    shared pipeline

--- vars/slacksuccess.groovy
+++ vars/slacksuccess.groovy
@@ -1,3 +1,6 @@
-def call () {
+def success () {
     slackSend color: "good", channel: 'U01SLG5TVQF', message: "Build Succeeded. The Job: ${env.JOB_NAME} has been built successfully with Build# ${env.BUILD_NUMBER} which triggered with change in ${env.BRANCH_NAME} Branch with Job URL: ${env.BUILD_URL}"
+}
+def failure () {
+    slackSend color: "danger", channel: 'U01SLG5TVQF', message: "Build Failed. The Job: ${env.JOB_NAME} has been failed with Build# ${env.BUILD_NUMBER} which triggered with change in ${env.BRANCH_NAME} Branch with Job URL: ${env.BUILD_URL}" 
 }
